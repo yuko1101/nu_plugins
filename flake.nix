30674e19ed9bd65ebf53a39aaf2547eba4cc6f75
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2025-05-12 16:42:25 +0900

    packed plugins with nix

--- flake.nix
+++ flake.nix
@@ -8,20 +8,50 @@
     };
   };
 
-  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = import nixpkgs { 
-          inherit system; 
+        pkgs = import nixpkgs {
+          inherit system;
           overlays = [ rust-overlay.overlays.default ];
         };
+        custom-rust-bin = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = custom-rust-bin;
+          rustc = custom-rust-bin;
+        };
+        p =
+          name:
+          rustPlatform.buildRustPackage rec {
+            pname = name;
+            version = (builtins.fromTOML (builtins.readFile "${src}/Cargo.toml")).package.version;
+
+            src = "${self}/${pname}";
+
+            cargoLock = {
+              lockFile = "${src}/Cargo.lock";
+            };
+          };
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+          packages = [
+            custom-rust-bin
           ];
         };
+        packages = pkgs.lib.foldl' (acc: name: acc // { "${name}" = p name; }) { } [
+          "nu_plugin_extra_parsers"
+          "nu_plugin_extras"
+          "nu_plugin_reverse_engineering"
+        ];
       }
     );
 }
